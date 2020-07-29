aecf60cd929e7ebcd42f59779b0e713eda931f7d
Daniel Lima <daniel.96.lima@gmail.com>
2020-07-29 12:40:22 -0300

    AJUSTES NO ENDPOINT / CATEGORIA / {ID}

--- src/main/java/com/nelioalves/cursomc/Services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/Services/CategoriaService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.nelioalves.cursomc.Services.exceptions.ObjectNotFoundException;
 import com.nelioalves.cursomc.domain.Categoria;
 import com.nelioalves.cursomc.repositories.CategoriaRepository;
 
@@ -18,6 +19,9 @@ public class CategoriaService {
 	
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! ID:" + id + ", Tipo: " + Categoria.class.getName());
+		}
 		return obj.orElse(null);
 	}
 	
---
+++ src/main/java/com/nelioalves/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.nelioalves.cursomc.resources.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpServerErrorException;
+
+import com.nelioalves.cursomc.Services.exceptions.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServerErrorException request){
+		
+		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+
+}
